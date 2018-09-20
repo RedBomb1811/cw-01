ad8cf92c08826c3a3fb8a7a24404e349ba6aa5a2
RedBomb1811 <dimadudko00@gmail.com>
2018-09-20 20:03:42 +0300

    Created summary.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-for(var i = 2; i < process.argv.length; i++)
-    console.log(`Parameter #${i-1}: ${process.argv[i]}`);
+var fs = require('fs');
+var path = require('path');
+var path = process.argv[2];
+let arr = [];
+getFiles(path);
+fs.writeFile(`${path}summary.js`, createSummaryFile(arr), function () {
+});
 
+function getFiles(p_path) {
+    let files = fs.readdirSync(p_path);
+    for (let i in files) {
+        let name = p_path + files[i];
+        if (fs.statSync(name).isDirectory()) {
+            name += '\\\\';
+            getFiles(name);
+        } else {
+            arr.push("\\\\" + (name).substring(path.length));
+        }
+    }
+}
+
+function createSummaryFile(p_arr) {
+    let str = "";
+    p_arr.forEach(function (item) {
+        str += `console.log(\"${item}\");\n`;
+    });
+    // for (var i in p_arr)
+    //     str += `Console.log(${i)});\n`;
+    return str;
+}
